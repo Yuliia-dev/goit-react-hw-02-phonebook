d3a943f0524f73af146d3184955345473f5e2ca3
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2022-01-17 15:42:40 +0200

    changes class to func

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   ContactsList,
@@ -7,36 +6,34 @@ import {
   ContactsBtn,
 } from './ContactsList.styled';
 
-export default class Contacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      }),
-    ),
-    deleteContact: PropTypes.func.isRequired,
-  };
-  render() {
-    const { contacts, deleteContact } = this.props;
-    return (
-      <>
-        <ContactsList>
-          {contacts.map(({ id, name, number }) => {
-            return (
-              <ContactsItem key={id}>
-                <ContactsItemText>
-                  {name}: {number}
-                </ContactsItemText>{' '}
-                <ContactsBtn onClick={() => deleteContact(id)}>
-                  Delete
-                </ContactsBtn>
-              </ContactsItem>
-            );
-          })}
-        </ContactsList>
-      </>
-    );
-  }
+export default function Contacts({ contacts, deleteContact }) {
+  return (
+    <>
+      <ContactsList>
+        {contacts.map(({ id, name, number }) => {
+          return (
+            <ContactsItem key={id}>
+              <ContactsItemText>
+                {name}: {number}
+              </ContactsItemText>{' '}
+              <ContactsBtn onClick={() => deleteContact(id)}>
+                Delete
+              </ContactsBtn>
+            </ContactsItem>
+          );
+        })}
+      </ContactsList>
+    </>
+  );
 }
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+  deleteContact: PropTypes.func.isRequired,
+};
